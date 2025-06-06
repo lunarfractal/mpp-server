41c00a88dc9d12ce4ead13b6776ae0df120a0bd9
lunarfractal <xxs239239@gmail.com>
2025-06-06 12:59:47 +0530

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -1,14 +1,14 @@
 #include <csignal>
 #include <functional>
 
-#include "network/network.hpp"
-#include "utils/utils.hpp"
-#include "game/game.hpp"
-
 #define ASIO_STANDALONE
 
 #include <websocketpp/config/asio_no_tls.hpp>
 #include <websocketpp/server.hpp>
+
+#include "network/network.hpp"
+#include "utils/utils.hpp"
+#include "game/game.hpp"
 
 
 using websocketpp::lib::bind;
@@ -30,12 +30,374 @@ public:
         m_server.set_message_handler(bind(&mpp_server::on_message,this,::_1,::_2));
 
         m_server.clear_access_channels(websocketpp::log::alevel::all);
+
+        alog = m_server.get_alog();
+        elog = m_server.get_elog();
     }
 
     void start_loop() {}
 
-    void process_message(std::string &payload, connection_hdl hdl) {
+    void process_message(std::string &buffer, connection_hdl hdl) {
+        auto s = m_sessions[hdl];
 
+        switch(buffer[0]) {
+            case network::opcode::ping:
+            {
+                alog.write(websocketpp::log::alevel::app, "ping!");
+                m_server.send(hdl, {0}, 1, websocketpp::frame::opcode::binary);
+                alog.write(websocketpp::log::alevel::app, "I sent a pong as a response.");
+                
+                if(!s->received_ping) s->received_ping = true;
+                
+                break;
+            }
+
+            case network::opcode::hello:
+            {
+                if(buffer.length() >= 5) {
+                    alog.write(websocketpp::log::alevel::app, "hello!");
+                    std::memcpy(&s->screen_width, &buffer[1], 2);
+                    std::memcpy(&s->screen_height, &buffer[3], 2);
+
+                    if(!s->received_hello) s->received_hello = true;
+
+                    s->type = network::session_type::player;
+
+                    if(s->screen_width == 0 || s->screen_height == 0) {
+                        alog.write(websocketpp::log::alevel::app, "screen 0x0, closing the connection");
+                        m_server.close(hdl, websocketpp::close::status::normal, "");
+                    }
+                } else {
+                    alog.write(websocketpp::log::alevel::app, "hello packet is too short... closing the connection.");
+                    m_server.close(hdl, websocketpp::close::status::normal, "");
+                }
+                break;
+            }
+
+            case network::opcode::hello_bot:
+            {
+                if(buffer.length() >= 5) {
+                    alog.write(websocketpp::log::alevel::app, "hello bot!");
+                    std::memcpy(&s->screen_width, &buffer[1], 2);
+                    std::memcpy(&s->screen_height, &buffer[3], 2);
+
+                    if(!s->received_hello) s->received_hello = true;
+
+                    s->type = network::session_type::bot;
+                    
+                    if(s->screen_width == 0 || s->screen_height == 0) {
+                        alog.write(websocketpp::log::alevel::app, "screen 0x0, closing the connection");
+                        m_server.close(hdl, websocketpp::close::status::normal, "");
+                    }
+                } else {
+                    alog.write(websocketpp::log::alevel::app, "hello packet is too short... closing the connection.");
+                    m_server.close(hdl, websocketpp::close::status::normal, "");
+                }
+                break;
+            }
+
+            case network::opcode::hello_debug:
+            {
+                if(buffer.length() >= 5) {
+                    alog.write(websocketpp::log::alevel::app, "hello debug!");
+                    std::memcpy(&s->screen_width, &buffer[1], 2);
+                    std::memcpy(&s->screen_height, &buffer[3], 2);
+
+                    int offset = 5;
+                    try {
+                        std::string pass = utils::getString(buffer, offset);
+                        if(pass == "plsadmin")
+                            s->type = network::session_type:dev;
+                        else
+                            s->type = network::session_type::player;
+                    } catch(std::out_of_range &e) {
+                        alog.write(websocketpp::log::alevel::app, "invalid debug packet, closing the connection");
+                        return;
+                    }
+
+                    if(!s->received_hello) s->received_hello = true;
+
+                    if(s->screen_width == 0 || s->screen_height == 0) {
+                        alog.write(websocketpp::log::alevel::app, "screen 0x0, closing the connection");
+                        m_server.close(hdl, websocketpp::close::status::normal, "");
+                    }
+                } else {
+                    alog.write(websocketpp::log::alevel::app, "hello packet is too short... closing the connection.");
+                    m_server.close(hdl, websocketpp::close::status::normal, "");
+                }
+                break;
+            }
+
+            case network::opcode::enter_game:
+            {
+                if(buffer.size() < 8) {
+                    alog.write(websocketpp::log::alevel::app, "invalid enter game packet! (too short), closing the connection");
+                    m_server.close(hdl, websocketpp::close::status::normal, "");
+                    
+                    return;
+                }
+
+                if(s->did_enter_game() || !s->did_send_hello()) {
+                    alog.write(websocketpp::log::alevel::app, "not the right time to enter the game! closing the connection");
+                    m_server.close(hdl, websocketpp::close::status::normal, "");
+                    
+                    return;
+                }
+                
+                auto p = std::make_shared<game::player>();
+                p->session = std::weak_ptr(s);
+                s->player = p;
+
+                switch(s->type) {
+                    case network::session_type::player:
+                    s->player->is_player = true;
+                    break;
+
+                    case network::session_type::bot:
+                    s->player->is_bot = true;
+                    break;
+
+                    case network::session_type::dev:
+                    s->player->is_dev = true;
+                    break;
+                }
+
+                p->id = game_world.add_player(p);
+
+                try {
+                    int offset = 1;
+                    
+                    p->red = buffer[offset++];
+                    p->green = buffer[offset++];
+                    p->blue = buffer[offset++];
+                    
+                    p->nick = utils::getString(buffer, offset);
+                    p->room_id = utils::getString(buffer, offset);
+
+                    if(p->room_id == "") p->room_id = "lobby";
+                    
+                } catch(std::out_of_range &e) {
+                    alog.write(websocketpp::log::alevel::app, "invalid enter game packet! closing the connection");
+                    m_server.close(hdl, websocketpp::close::status::normal, "");
+                }
+                
+                break;
+            }
+
+            case network::opcode::leave_game:
+            {
+                if(!s->did_enter_game()) {
+                    alog.write(websocketpp::log::alevel::app, "can't leave the game without entering it first");
+                    return;
+                }
+
+                s->player->deletion_reason = 0x03;
+                game_world.mark_for_deletion(s->player->id);
+                break;
+            }
+
+            case network::opcode::resize:
+            {
+                if(buffer.length() >= 5) {
+                    alog.write(websocketpp::log::alevel::app, "resize!");
+                    std::memcpy(&s->screen_width, &buffer[1], 2);
+                    std::memcpy(&s->screen_height, &buffer[3], 2);
+
+                    if(s->screen_width == 0 || s->screen_height == 0) {
+                        alog.write(websocketpp::log::alevel::app, "screen 0x0, closing the connection");
+                        m_server.close(hdl, websocketpp::close::status::normal, "");
+                    }
+                } else {
+                    alog.write(websocketpp::log::alevel::app, "resize packet is too short... closing the connection.");
+                    m_server.close(hdl, websocketpp::close::status::normal, "");
+                }
+                break;
+            }
+
+            case network::opcode::input:
+            {
+                if(!s->did_enter_game()) {
+                    alog.write(websocketpp::log::alevel::app, "can't move your cursor without entering the game first");
+                    return;
+                }
+                
+                if(buffer.length() >= 5) {
+                    std::memcpy(&s->player->x, &buffer[1], 2);
+                    std::memcpy(&s->player->y, &buffer[3], 2);
+                } else {
+                    alog.write(websocketpp::log::alevel::app, "cursor packet is too short... closing the connection.");
+                    m_server.close(hdl, websocketpp::close::status::normal, "");
+                }
+                
+                break;
+            }
+
+            case network::opcode::nick:
+            {
+                if(!s->did_enter_game()) {
+                    alog.write(websocketpp::log::alevel::app, "can't change your nick without entering the game first");
+                    return;
+                }
+
+                if(buffer.size() > 1 + 2 && buffer.size() < 1 + 15 + 3) {
+                    try {
+                        int offset = 1;
+                        p->nick = utils::getString(buffer, offset);
+                    } catch(std::out_of_range &e) {
+                        alog.write(websocketpp::log::alevel::app, "invalid nick packet! closing the connection");
+                        m_server.close(hdl, websocketpp::close::status::normal, "");
+                    }
+                } else {
+                    alog.write(websocketpp::log::alevel::app, "invalid nick packet (too short).");
+                    m_server.close(hdl, websocketpp::close::status::normal, "");
+                }
+                
+                break;
+            }
+
+            case network::opcode::color:
+            {
+                if(!s->did_enter_game()) {
+                    alog.write(websocketpp::log::alevel::app, "can't change your color without entering the game first");
+                    return;
+                }
+
+                if(buffer.size() == 1 + 3) {
+                    int offset = 1;
+                    s->player->red = buffer[offset++];
+                    s->player->green = buffer[offset++];
+                    s->player->blue = buffer[offset++];
+                }
+                
+                break;
+            }
+
+            case network::opcode::chat:
+            {
+                if(!s->did_enter_game()) {
+                    alog.write(websocketpp::log::alevel::app, "can't chat without entering the game first");
+                    return;
+                }
+
+                if(buffer.size() > 1 + 500 + 3) {
+                    alog.write(websocketpp::log::alevel::app, "message too long!");
+                    return;
+                }
+
+                try {
+                    std::string chat_message = utils::getString(buffer, offset);
+                    if(chat_message == "") {
+                        alog.write(websocketpp::log::alevel::app, "null message!");
+                        return;
+                    }
+
+                    // TODO dispatch
+
+                    game_world.add_message(s->player->room_id, {
+                        chat_message,
+                        s->player->nick,
+                        s->player->hue,
+                        s->player->id,
+                        std::chrono::duration<double>(std::chrono::system_clock::now().time_since_epoch()).count()
+                    });
+                } catch(std::out_of_range &e) {
+                    alog.write(websocketpp::log::alevel::app, "Invalid message! closing connection");
+                    m_server.close(hdl, websocketpp::close::status::normal, "");
+                }
+                
+                break;
+            }
+
+            case network::opcode::change_room:
+            {
+                if(!s->did_enter_game()) {
+                    alog.write(websocketpp::log::alevel::app, "can't change your room without entering the game first");
+                    return;
+                }
+
+                try {
+                    int offset = 1;
+                    std::string room_id = utils::getString(buffer, offset);
+                    if(room_id == "") {
+                        alog.write(websocketpp::log::alevel::app, "null room id! closing connection");
+                        m_server.close(hdl, websocketpp::close::status::normal, "");
+                        return;
+                    } else {
+                        s->player->room_id = room_id;
+                    }
+                } 
+                catch(std::out_of_range &e) {
+                    alog.write(websocketpp::log::alevel::app, "Invalid message! closing connection");
+                    m_server.close(hdl, websocketpp::close::status::normal, "");
+                }
+                
+                break;
+            }
+
+            case network::opcode::update_room:
+            {
+                if(!s->did_enter_game()) {
+                    alog.write(websocketpp::log::alevel::app, "can't update your room without entering the game first");
+                    return;
+                }
+                
+                break;
+            }
+
+            case network::opcode::delete_room:
+            {
+                if(!s->did_enter_game()) {
+                    alog.write(websocketpp::log::alevel::app, "can't delete your room without entering the game first");
+                    return;
+                }
+                break;
+            }
+
+            case network::opcode::note:
+            {
+                if(!s->did_enter_game()) {
+                    alog.write(websocketpp::log::alevel::app, "can't play notes without entering the game first");
+                    return;
+                }
+                
+                break;
+            }
+
+            case network::opcode::debug_ban:
+            {
+                if(!s->did_enter_game()) {
+                    alog.write(websocketpp::log::alevel::app, "can't ban without entering the game first");
+                    return;
+                }
+                
+                break;
+            }
+
+            case network::opcode::debug_mute:
+            {
+                if(!s->did_enter_game()) {
+                    alog.write(websocketpp::log::alevel::app, "can't mute without entering the game first");
+                    return;
+                }
+                break;
+            }
+
+            case network::opcode::debug_kick:
+            {
+                if(!s->did_enter_game()) {
+                    alog.write(websocketpp::log::alevel::app, "can't kick without entering the game first");
+                    return;
+                }
+                
+                break;
+            }
+            
+            default:
+            {
+                
+                break;
+            }
+        }
     }
 
     void on_open(connection_hdl hdl) {
@@ -70,9 +432,12 @@ public:
     }
 
 private:
-    game::game_manager game_world;
-
     server m_server;
+
+    websocketpp::log::logger<websocketpp::log::alevel::log> alog;
+    websocketpp::log::logger<websocketpp::log::elevel::log> elog;
+    
+    game::game_manager game_world;
 
     typedef struct {
         std::size_t operator()(const websocketpp::connection_hdl& hdl) const {
@@ -86,41 +451,7 @@ private:
         }
     } connection_hdl_equal;
 
-
     std::unordered_map<connection_hdl, std::shared_ptr<network::session>, connection_hdl_hash, connection_hdl_equal> m_sessions;
-
-    void pong(connection_hdl hdl) {
-        uint8_t buffer[] = {net::opcode_pong};
-        try {
-            m_server.send(hdl, buffer, sizeof(buffer), websocketpp::frame::opcode::binary);
-        } catch (websocketpp::exception const & e) {
-            std::cout << "Pong failed because: "
-                << "(" << e.what() << ")" << std::endl;
-        }
-    }
-
-    void sendId(connection_hdl hdl, uint16_t id, uint16_t hue) {
-        uint8_t buffer[5];
-        buffer[0] = net::opcode_entered_game;
-        std::memcpy(&buffer[1], &id, sizeof(uint16_t));
-        std::memcpy(&buffer[3], &hue, 2);
-        try {
-            m_server.send(hdl, buffer, sizeof(buffer), websocketpp::frame::opcode::binary);
-        } catch (websocketpp::exception const & e) {
-            std::cout << "Send failed because: "
-                << "(" << e.what() << ")" << std::endl;
-        }
-    }
-
-    void sendBuffer(connection_hdl hdl, uint8_t *buffer, size_t size) {
-        try {
-            if (m_server.get_con_from_hdl(hdl)->get_state() == websocketpp::session::state::open)
-                m_server.send(hdl, buffer, size, websocketpp::frame::opcode::binary);
-        } catch (websocketpp::exception const & e) {
-            std::cout << "Send failed because: "
-                << "(" << e.what() << ")" << std::endl;
-        }
-    }
 };
 
 
