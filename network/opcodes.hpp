caedc6d45b302a5f71c897b98d1ef25f6d070045
lunarfractal <xxs239239@gmail.com>
2025-06-06 11:21:01 +0530

    Update opcodes.hpp

--- network/opcodes.hpp
+++ network/opcodes.hpp
@@ -39,6 +39,32 @@ constexpr uint8_t notes = 0xA7;
 
 } // opcode
 
+namespace decoded {
+
+constexpr uint8_t close = 0xEE;
+constexpr uint8_t send_pong = 0xE1;
+constexpr uint8_t send_id = 0xE2;
+constexpr uint8_t encode_screen_ffs = 0xE3;
+constexpr uint8_t add_bot_mark = 0xE4;
+constexpr uint8_t add_dev_mark = 0xE9;
+constexpr uint8_t add_to_game = 0xE5;
+constexpr uint8_t delete_from_game = 0xE6;
+constexpr uint8_t encode_screen = 0xE7;
+constexpr uint8_t encode_cursor = 0xE8;
+constexpr uint8_t encode_nick = 0xEA;
+constexpr uint8_t encode_hue = 0xEB;
+constexpr uint8_t encode_color = 0xEC;
+constexpr uint8_t add_message = 0xED;
+constexpr uint8_t change_room = 0xEF;
+constexpr uint8_t update_room = 0xE0;
+constexpr uint8_t delete_room = 0xF0;
+constexpr uint8_t play_note = 0xF1;
+constexpr uint8_t ban = 0xF2;
+constexpr uint8_t mute = 0xF3;
+constexpr uint8_t kick = 0xF4;
+
+}
+
 namespace session_type {
 
 constexpr uint8_t player = 0xD1;
@@ -75,8 +101,8 @@ constexpr uint8_t updated_nick = 0x16;
 constexpr uint8_t updated_hue = 0x17;
 constexpr uint8_t updated_color = 0x18;
 constexpr uint8_t created_room = 0x19;
-constexpr uint8_t updated_room = 0x20;
-constexpr uint8_t deleted_room = 0x21;
+constexpr uint8_t updated_room = 0x1A;
+constexpr uint8_t deleted_room = 0x1B;
 
 } // event
 
